97c008904bff325357cc12d01bd6f2ddc3860ad0
Mikey-mw <mikeywubshet@gmail.com>
2023-10-06 14:49:23 -0700

    commit 3

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int number;
+	int number; /* Declare variable for the number */
 
-for (number = 0; number < 10; number++)
-{
-printf("%d", number);
-}
-    
-printf("\n");
+	/* Loop through numbers from 0 to 9 */
+	for (number = 0; number < 10; number++)
+	{
+		/* Print the current number */
+		putchar(number + '0');
+	}
 
-return (0);
+	/* Print a newline character for readability */
+	putchar('\n');
+
+	return (0);
 }
 
 
