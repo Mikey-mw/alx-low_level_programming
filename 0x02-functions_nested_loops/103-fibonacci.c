c684449725d794f00702df3f1aa2986a71c8614f
Mikey-mw <mikeywubshet@gmail.com>
2023-10-11 14:09:20 -0700

    my last commit 1

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-
 /**
-  * main - the main printing funtion
-  * less than 4000000.
-  * Return: 0.
-  */
-
-
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+    unsigned long first = 1, second = 2, next, sum = 0;
 
+    while (second <= 4000000)
+    {
+        if (second % 2 == 0)
+        {
+            sum += second;
+        }
+        next = first + second;
+        first = second;
+        second = next;
+    }
 
-	while (k + j < 4000000)
-	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
-	}
-	printf("%ld", sum);
-	return (0);
-}
+    printf("%lu\n", sum);
+
+    return (0);
 
